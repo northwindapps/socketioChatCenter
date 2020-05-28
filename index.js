5779c48e4b98d99edc3c0a16e5f5c358e457ba8f
Yujin Yano <northwindapps@gmail.com>
2020-05-29 01:20:14 +0900

    mongodb introduced

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
+const mongo = require("mongodb").MongoClient;
 const express = require('express');
 let app = require('express')();
 const path = require('path');
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
 let port = process.env.PORT || 3001;
+
+mongo.connect("mongodb://localhost:27017/customercalls", {useNewUrlParser: true, useUnifiedTopology: true},(error,client)=>{
+    if (error) {
+        throw error;
+    }
+
+    console.log("mongdb connected..");
+    let caseListStr= "";
+    let db = client.db('customercalls');
+    let list = db.collection('unhandledcases');
+    list.countDocuments().then((res)=>{
+      let itemCounter = res;
+        if(itemCounter == 0)
+        {
+            //writing document. initialization
+            list.insertOne({_id:1, case: ','});
+        }
+    }); 
+
+    list.find().forEach(function(doc, err){
+        if (doc) {
+          console.log("first item",doc.case);
+          caseListStr = doc.case;
+        }  
+    });
+
+
+
 
 app.use(express.static(path.join(__dirname,'public')));
 
@@ -12,9 +41,22 @@ io.on('connection', function(socket){
   io.sockets.emit('to operator', 'readyToJoin');  
 
   socket.on('customer joined', function(msg){
-    
-    io.sockets.emit('update customer list',msg);
-    
+    caseListStr = caseListStr + ',' + msg;
+    console.log("list updated",caseListStr);
+    list.countDocuments().then((res)=>{
+      let itemCounter = res;
+      if(itemCounter > 0){
+          list.updateOne(
+              {_id:1},
+              {
+                  $set:{
+                      case: caseListStr
+                  }
+              }
+          )
+        }
+      }); 
+    io.sockets.emit('update customer list');
   });
 
   socket.on('chat message', function(data){
@@ -28,9 +70,29 @@ io.on('connection', function(socket){
 
   socket.on('leave', function(msg) {
     socket.leave(msg);
+    let caselist = caseListStr.split(',');
+    caselist = caselist.filter(e => e !== msg);
+    caselist = caselist.filter(function(str) {
+      return /\S/.test(str);
+    });
+    caseListStr = caselist.join(',');
+    list.countDocuments().then((res)=>{
+      let itemCounter = res;
+      if(itemCounter > 0){
+        console.log("list updated",caseListStr);
+        
+          list.updateOne(
+              {_id:1},
+              {
+                  $set:{
+                      case: caseListStr
+                  }
+              }
+          )
+        }
+      }); 
+    io.sockets.emit('update customer list');
   });
-
-  
 });
 
 app.get('/op', function(req, res){
@@ -41,6 +103,17 @@ app.get('/hq', function(req, res){
   res.sendFile(__dirname + '/public/hq.html');
 });
 
+app.get('/caselist', function(req, res){
+  list.find().forEach(function(doc, err){
+    if (doc) {
+      caseListStr = doc.case;
+    }  
+  });
+  res.send({msg:caseListStr});
+});
+
 http.listen(port, function(){
   console.log('listening on *:' + port);
+});
+
 });
